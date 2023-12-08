5452bac903eb49e4ac2385322eb5777ce25b4ebd
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2023-12-08 13:55:32 -0700

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [quote, setQuote] = useState('Quote')
   const [author, setAuthor] = useState('Author')
 
+  const max = 150
 const Quote = () => {
   let url = 'https://api.quotable.io/random'
   fetch(url).then((response) => {
@@ -12,8 +13,11 @@ const Quote = () => {
         throw new Error(response.statusText)
     }
 }).then((quote) => {
-  setQuote(quote.content)
-  setAuthor(`- ${quote.author}`)
+  if(quote.length <= max){
+    setQuote(quote.content)
+    setAuthor(`- ${quote.author}`)
+  }
+   
 }).catch((error) => { console.log(error) })
 }
 
@@ -23,13 +27,21 @@ useEffect(() => {
 }, [])
 
     return (
-      <div>
-        <div className='box'>
-            <h1>{quote}</h1>
-            <br></br>
-            <h2><button onClick={() => {Quote()}}><FiRefreshCw /></button>{author}</h2>
-        </div>
-      </div>
+      <>
+        <div>
+          
+          <div class="container">
+          <img class="fog" src="https://64.media.tumblr.com/224f9198d5d88a5e92e43f5ef4f7a592/139ff9bb70edd708-66/s540x810/0edbf6d586b6b4c85c5bd61569992f036c21191b.png" alt="fog1.png"/>
+          </div>
+
+          <div className='box'>
+              <h1 className='fade-in-text quote-box'>{quote}</h1>
+              <br></br>
+              <h2 className='fade-in-text'><button onClick={() => {Quote()}}><FiRefreshCw /></button>{author}</h2>
+          </div>
+        </div>     
+      </>
+      
     )
 }
 export default App;
